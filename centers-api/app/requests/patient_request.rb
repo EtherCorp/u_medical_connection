7c0c0d2b640a545553269852823ce9083b059c6d
kevin-alvarez <kebingo12@gmail.com>
2017-10-09 19:51:49 -0300

    GET POST DELETE and patient Request

---
+++ centers-api/app/requests/patient_request.rb
@@ -0,0 +1,4 @@
+class PatientRequest < Request
+  @entity = "patients"
+
+end
--- centers-api/app/requests/request.rb
+++ centers-api/app/requests/request.rb
@@ -1,7 +1,20 @@
 class Request
   require "http"
-  def require ""
-    url = "www.google.com"
-    req = HTTP.get(url)
+
+  @@url = "localhost:3000/api/v1/"
+
+  def self.GET(id = nil)
+    dir = @@url + @entity + "/" + id.to_s
+    puts HTTP.get(dir)
+  end
+
+  def self.POST(id)
+    dir = @@url + @entity + "/" + id.to_s
+    req = HTTP.post(dir)
+  end
+
+  def self.DELETE(id)
+    dir = @@url + @entity + "/" + id.to_s
+    req = HTTP.delete(dir)
   end
 end
