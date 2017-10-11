b4a93ac04d3f58f1f2785bfcc27d75d2f9b61167
Gerardo Zúñiga <monodeth@gmail.com>
2017-10-11 11:01:19 -0300

    Base structure of Driver module

---
+++ centers-api/lib/drivers/base.rb
@@ -0,0 +1,10 @@
+module Drivers
+  # Base class of all Drivers.
+  class Base
+    # Dispatch message depending of request_type
+    def parse(request_type, request)
+      method_name = "parse_#{request_type}"
+      public_send(method_name, request)
+    end
+  end
+end
---
+++ centers-api/lib/drivers/medical_center1.rb
@@ -0,0 +1,9 @@
+module Drivers
+  # Driver for Medical Center 1
+  # This parse the request to a normalized json-like hash
+  class MedicalCenter1 < Base
+    def parse_professional_information(request)
+      request
+    end
+  end
+end
