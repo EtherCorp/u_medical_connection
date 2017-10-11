fe6c7f152bbf8b9c607036ef1a524e0551a0d284
Israel <israel.martinez@usach.cl>
2017-10-11 01:33:59 -0300

    concept test workers sidekiq (Polanco, Martínez)

--- centers-api/app/controllers/report_worker.rb
+++ centers-api/app/controllers/report_worker.rb
@@ -3,5 +3,7 @@ class ReportWorker
   sidekiq_options retry: false
   def perform(token, medical_center, title, body)
     puts "SIDEKIQ WORKER GENERATING A REPORT FROM #{"medical_center"} WITH #{"token"}, #{"title"} and #{"body"}"
+    puts 'translate = driver(data)'
+    puts 'POST unicorn(translate)'
   end
 end
