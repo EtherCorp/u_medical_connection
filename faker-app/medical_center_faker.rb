69dd5d62b53badf600ea237db2103d6715bed348
Gerardo Zúñiga <monodeth@gmail.com>
2017-10-11 12:50:09 -0300

    Faker App refactored

---
+++ faker-app/medical_center_faker.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'json'
+require 'net/http'
+require 'uri'
+Dir[File.expand_path 'centers/**/*.rb'].each{|f| require_relative(f)}
+
+class MedicalCenterFaker
+  attr_reader :uri
+
+  def initialize(target_uri = 'http://localhost:3001/api/v1/')
+    @uri = URI.parse(target_uri)
+  end
+
+  def post_request(endpoint, body)
+    header = {'Content-Type': 'application/json'}
+    http = Net::HTTP.new(uri.host<<endpoint, uri.port)
+    request = Net::HTTP::Post.new(uri.path,header)
+    request.body = body.to_json
+    response = http.request(request)
+    puts response
+  end
+
+  def run(requests=[{source:'MedicalCenter1', request_type:'patient', request_number:10}])
+    requests.each do |request|
+      (1..request[:request_number]).each do
+        request_type = request[:request_type]
+        request_source = request[:source]
+        request_body = Object.const_get(request_source).public_send(request_type)
+        puts request_body #Print the result
+        post_request(request_type<<'s',request_body)
+      end
+    end
+  end
+
+end
+
