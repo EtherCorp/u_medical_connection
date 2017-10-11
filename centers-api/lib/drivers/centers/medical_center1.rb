3be50d005268fc9d3b3be47c526843680a5c7d39
Gerardo Zúñiga <monodeth@gmail.com>
2017-10-11 14:52:25 -0300

    Fix in rut parsing

--- centers-api/lib/drivers/centers/medical_center1.rb
+++ centers-api/lib/drivers/centers/medical_center1.rb
@@ -5,7 +5,7 @@ module Drivers
     def parse_patients(request)
       parsed = {}
       rut_string = request[:run].to_s
-      parsed[:rut] = rut_string[0..-2] << '-' << rut_string[-1]
+      parsed[:rut] = rut_string[0..-2] + '-' + rut_string[-1]
       parsed[:name], parsed[:last_name] = request[:nombre].split(' ', 2)
       parsed[:age] = request[:edad]
       parsed
