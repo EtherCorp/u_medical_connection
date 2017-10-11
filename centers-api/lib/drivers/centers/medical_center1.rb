1be0e4e76368d24f5c896244564c8829f320df59
Gerardo Zúñiga <monodeth@gmail.com>
2017-10-11 14:04:13 -0300

    Minor changes
    - Tagged old driver as deprecated and saved for reference
    - Parsed rut as String

--- centers-api/lib/drivers/centers/medical_center1.rb
+++ centers-api/lib/drivers/centers/medical_center1.rb
@@ -4,7 +4,8 @@ module Drivers
   class MedicalCenter1 < Base
     def parse_patients(request)
       parsed = {}
-      parsed[:rut] = request[:run]
+      rut_string = request[:run].to_s
+      parsed[:rut] = rut_string[0..-2] << '-' << rut_string[-1]
       parsed[:name], parsed[:last_name] = request[:nombre].split(' ', 2)
       parsed[:age] = request[:edad]
       parsed
